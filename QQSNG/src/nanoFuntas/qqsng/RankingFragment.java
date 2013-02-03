e6fb0e597051484f33bf43f634b07c8132330d90
WU YONGSHOU <footballlifer@hotmail.com>
2013-02-03 20:33:12 +0900

    sorting listView according to score is done

--- QQSNG/src/nanoFuntas/qqsng/RankingFragment.java
+++ QQSNG/src/nanoFuntas/qqsng/RankingFragment.java
@@ -10,6 +10,9 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ListView;
 
+/**
+ * This RankingFragment class is used by MainActivity to to show listView
+ */
 public class RankingFragment extends Fragment {
 	private final static boolean DEBUG = true;
 	private final static String TAG = "RankingFragment";
@@ -24,9 +27,32 @@ public class RankingFragment extends Fragment {
     	mListView = (ListView) getView().findViewById(R.id.listView1);
 
         Bundle bundle = getArguments();       
-        ArrayList<PhotoTextItem> mItemList = bundle.getParcelableArrayList("gamerList");
+        ArrayList<PhotoTextItem> mItemList = bundle.getParcelableArrayList("gamerList");        
+        sortListView(mItemList);
     	PhotoTextListAdapter mPhotoTextListAdapter = new PhotoTextListAdapter(getActivity(), mItemList);
         mListView.setAdapter(mPhotoTextListAdapter);                        
+	}
+
+	/**
+	 * This sortListView method is used to sort listView according to score.
+	 * @param mItemList listView needs to be sorted.
+	 */
+	private void sortListView(ArrayList<PhotoTextItem> mItemList) {
+		PhotoTextItem pti = null;
+        int sizeOfmItemList = mItemList.size();
+        for(int i=0; i<sizeOfmItemList-1; i++){
+			for(int j=0; j<sizeOfmItemList-1-i; j++){
+				Long l1 = Long.parseLong(mItemList.get(j).getScore());
+				Long l2 = Long.parseLong(mItemList.get(j+1).getScore());
+        	
+				if(l1 < l2){
+					// change two elements
+					pti = mItemList.get(j);
+					mItemList.set(j, mItemList.get(j+1));
+					mItemList.set(j+1, pti);
+				}
+			}
+		}
 	}
 	
 	@Override
